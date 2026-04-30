5635870dd2eab51a83c40fc728d9ca22c249ec41
Elevennails <simon.pytches@gmail.com>
2026-04-30 23:30:41 +0100

    Update install.sh
    made the apt install auto run without promting for y

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
-sudo apt install git gcc make unzip ripgrep fd-find git xclip neovim
+sudo apt install git gcc make unzip ripgrep fd-find git xclip neovim -y
 git clone https://github.com/Elevennails/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim

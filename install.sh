c8ad1a508358735754816fca3d1c589275c7887b
Elevennails <simon.pytches@gmail.com>
2026-05-01 15:37:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt install git gcc make unzip ripgrep fd-find git xclip neovim -y
 git clone https://github.com/Elevennails/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
